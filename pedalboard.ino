7a56dc2c9577fbfe9a835a6b2c7da914ec7e67a8
Tom Verhaeghe <info@heartdriven.com>
2023-11-02 11:25:26 +0100

    Adding support for 25 keys.

--- pedalboard.ino
+++ pedalboard.ino
@@ -2,7 +2,7 @@
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-int mem[] = {0, 0, 0, 0, 0};
+int mem[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
@@ -12,26 +12,75 @@ void setup() {
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+  pinMode(17, INPUT_PULLUP);
+  pinMode(18, INPUT_PULLUP);
+  pinMode(19, INPUT_PULLUP);
+  pinMode(20, INPUT_PULLUP);
+  pinMode(21, INPUT_PULLUP);
+  pinMode(22, INPUT_PULLUP);
+  pinMode(23, INPUT_PULLUP);
+  pinMode(24, INPUT_PULLUP);
+  pinMode(25, INPUT_PULLUP);
+  pinMode(26, INPUT_PULLUP);
+  pinMode(27, INPUT_PULLUP);
+  pinMode(28, INPUT_PULLUP);
+  
 
-  pinMode(13, OUTPUT);
+  pinMode(30, OUTPUT);
 }
 
 void loop() {
   int sensorVal[] = {
-    !digitalRead(2), !digitalRead(3), !digitalRead(4), !digitalRead(5), !digitalRead(6)
+    !digitalRead(2),
+    !digitalRead(3),
+    !digitalRead(4),
+    !digitalRead(5),
+    !digitalRead(6),
+    !digitalRead(7),
+    !digitalRead(8),
+    !digitalRead(9),
+    !digitalRead(10),
+    !digitalRead(11),
+    !digitalRead(12),
+    !digitalRead(13),
+    !digitalRead(14),
+    !digitalRead(15),
+    !digitalRead(16),
+    !digitalRead(17),
+    !digitalRead(18),
+    !digitalRead(19),
+    !digitalRead(20),
+    !digitalRead(21),
+    !digitalRead(22),
+    !digitalRead(23),
+    !digitalRead(24),
+    !digitalRead(25),
+    !digitalRead(26),
+    !digitalRead(27),
+    !digitalRead(28)
   };
 
-  for (int j = 0; j < 5; j++) {
+  for (int j = 0; j < 27; j++) {
     if (sensorVal[j] != mem[j]) {
       mem[j] = sensorVal[j];
 
         if (mem[j]) {
           MIDI.sendNoteOn(j + 48, 64, 1);
-          digitalWrite(13, HIGH);
+          digitalWrite(30, HIGH);
           
         } else {
           MIDI.sendNoteOff(j + 48, 64, 1);
-          digitalWrite(13, LOW);
+          digitalWrite(30, LOW);
         }
     }
   }
